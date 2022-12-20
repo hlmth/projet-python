cef90f2af80c619374af4912cc54f0feb87ec4c8
hlmth <hugolamothe8@gmail.com>
2022-12-20 23:08:52 +0100

    Création module qui regroupe les fonctions

---
+++ module_projet.py
@@ -0,0 +1,91 @@
+from geopy.geocoders import Nominatim
+import pandas as pd
+import geopandas as gpd
+
+from math import sin, cos, acos, pi
+  
+pd.options.mode.chained_assignment = None  # default='warn'
+
+
+### Définition des dataframes avec leurs colonnes "utiles"
+def data_carb(df, carb):
+    data_1 = df[['adresse', 'ville', 'geom', 'prix_valeur', 'prix_nom']]
+    d = data_1[data_1.prix_nom == carb]
+    d.drop_duplicates(keep = 'first', inplace=True) 
+    #df.drop_duplicates(subset ="'column'", keep = 'first', inplace=True) si on veut supprimer selon une colonne
+    return d
+
+
+def data_carb_g(df, carb):
+    data_1 = df[['adresse', 'ville', 'prix_valeur', 'prix_nom', 'geometry']]
+    d = data_1[data_1.prix_nom == carb]
+    d.drop_duplicates(keep = 'first', inplace=True) 
+    #df.drop_duplicates(subset ="'column'", keep = 'first', inplace=True) si on veut supprimer selon une colonne
+    return d
+
+### Fonction pour passer de coordonnées GPS à une adresse et inversement
+
+def Adresse_to_gps(Adresse) : 
+    geolocator = Nominatim(user_agent="Projet Python")
+    location = geolocator.geocode(Adresse)
+    return str(str(location.latitude) + ','+ str(location.longitude))
+
+def Gps_to_adresse(coord):
+    geolocator = Nominatim(user_agent="Projet Python")
+    location = geolocator.reverse(coord)
+    return location 
+
+
+### Fonctions pour calculer la distance entre 2 points à partir de coordonnées GPS
+def dms2dd(d, m, s):
+    """Convertit un angle "degrés minutes secondes" en "degrés décimaux"
+    """
+    return d + m/60 + s/3600
+ 
+def dd2dms(dd):
+    """Convertit un angle "degrés décimaux" en "degrés minutes secondes"
+    """
+    d = int(dd)
+    x = (dd-d)*60
+    m = int(x)
+    s = (x-m)*60
+    return d, m, s
+ 
+def deg2rad(dd):
+    """Convertit un angle "degrés décimaux" en "radians"
+    """
+    return dd/180*pi
+ 
+
+def rad2deg(rd):
+    """Convertit un angle "radians" en "degrés décimaux"
+    """
+    return rd/pi*180
+ 
+#############################################################################
+def distanceGPS(p1, p2):
+    """Retourne la distance en mètres entre les 2 points A et B connus grâce à
+       leurs coordonnées GPS (en radians).
+    """
+    latA, longA = p1
+    latB, longB = p2
+    # Rayon de la terre en mètres (sphère IAG-GRS80)
+    RT = 6378137
+    # angle en radians entre les 2 points
+    S = acos(sin(latA)*sin(latB) + cos(latA)*cos(latB)*cos(abs(longB-longA)))
+    # distance entre les 2 points, comptée sur un arc de grand cercle
+    return S*RT
+
+### Fonctions finales 
+
+def dix_plus_proche_card(df, posi, carb):
+    data_1 = data_carb(df, carb).sort_values('prix_valeur')
+    pos = list(map(float,Adresse_to_gps(posi).split(',')))
+    def distancetoposi(p2):
+        pos_carb = list(map(float, p2.split(',')))
+        return distanceGPS(pos, pos_carb)
+    new_df = data_1['geom'].apply(distancetoposi).sort_values().head(10)
+    return data_1.loc[new_df.index.to_list()]
+
+
+
